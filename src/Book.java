449fd1ea011cf86cdb695c85083860726f3017cb
Agresh agrawal <65882105+agresh312004@users.noreply.github.com>
2026-09-09 18:17:17 +0530

    Add Book class

---
+++ src/Book.java
@@ -0,0 +1,40 @@
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean issued;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.issued = false;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isIssued() {
+        return issued;
+    }
+
+    public void setIssued(boolean issued) {
+        this.issued = issued;
+    }
+
+    public void displayBook() {
+        System.out.println("Book ID: " + bookId +
+                ", Title: " + title +
+                ", Author: " + author +
+                ", Status: " + (issued ? "Issued" : "Available"));
+    }
+}

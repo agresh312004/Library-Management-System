b7bb28934ad1844743422ca5f95b4650e5de4e88
Agresh agrawal <65882105+agresh312004@users.noreply.github.com>
2026-09-09 18:17:33 +0530

    Add LibraryMember abstraction

---
+++ src/LibraryMember.java
@@ -0,0 +1,19 @@
+public abstract class LibraryMember {
+    private int memberId;
+    private String name;
+
+    public LibraryMember(int memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void showMemberType();
+}
